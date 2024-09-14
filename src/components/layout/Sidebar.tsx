86989aab9f49f11c1d4d049d75b0051d9203ce4f
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-09-15 02:26:56 +0400

    feat: add formatTitleToRoute func in utils n handle Sidebar link w it

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -3,6 +3,7 @@
 import { useQuery } from "convex/react";
 import { api } from "@/../convex/_generated/api";
 import Link from "next/link";
+import { formatTitleToRoute } from "@/lib/utils";
 
 export default function Sidebar({}) {
 	const chats = useQuery(api.chats.get);
@@ -10,15 +11,20 @@ export default function Sidebar({}) {
 	return (
 		<aside className="w-64 bg-gray-900 h-screen p-4">
 			<h2 className="text-xl font-bold mb-4">Your Chats</h2>
-			<ul>
-				{chats ? (
+			<ul className="flex flex-col gap-3">
+				{chats?.length ? (
 					chats.map((chat) => (
-						<li key={chat.id} className="mb-2">
-							<Link href={`/chat/${chat.id}`}>{chat.title}</Link>
+						<li key={chat.id}>
+							<Link
+								className="block bg-gray-700 hover:bg-gray-600 rounded-md px-3 py-2 w-full transition-colors"
+								href={`/chat/${formatTitleToRoute(chat.id)}`}
+							>
+								{chat.title}
+							</Link>
 						</li>
 					))
 				) : (
-					<></>
+					<li className="text-gray-400 italic">No chats available</li>
 				)}
 			</ul>
 		</aside>
