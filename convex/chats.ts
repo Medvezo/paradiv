62b67f3e5c053bdcf11742cf241b2fc5d2a3b7a3
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-09-15 21:10:13 +0400

    feat: add getById function in /convex/chats.ts

--- convex/chats.ts
+++ convex/chats.ts
@@ -1,8 +1,20 @@
 import { query } from "./_generated/server";
+import { v } from "convex/values";
 
 export const get = query({
-  args: {},
-  handler: async (ctx) => {
-    return await ctx.db.query("chats").collect();
-  },
+	args: {},
+	handler: async (ctx) => {
+		return await ctx.db.query("chats").collect();
+	},
+});
+
+export const getById = query({
+	args: { _id: v.string() },
+	handler: async (ctx, {_id}) => {
+		const chat = await ctx.db
+			.query("chats")
+			.filter((q) => q.eq(q.field("_id"), _id))
+			.first();
+		return chat;
+	},
 });
